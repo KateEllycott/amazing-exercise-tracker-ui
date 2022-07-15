e13c4c8e27664c4bccfe68e656b6ab5cf7563b8c
Ekaterina Sivakova <brannaminskugga@gmail.com>
2022-07-15 22:11:30 +0300

    Fetch Training Templates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      trainingTemplates: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/template/training-session')
+      .then(responce => responce.json())
+      .then(templates => this.setState(() => {
+        return { trainingTemplates: templates }
+      }));
+  }
+
+  render() {
+    const filteredList = this.state.trainingTemplates.filter((template) => {
+      return template.name.toLocaleLowerCase().includes(this.state.searchField)});
+
+    return (
+      <div className="App">
+        <input className='search-template' type='search' placeholder='Search template'
+          onChange={(event) => {
+            this.setState({searchField: event.target.value.toLocaleLowerCase()})}}/>
+        {filteredList.map(trainingTemplate => {
+          return <div key={trainingTemplate.id}>
+            <h1>{trainingTemplate.name}</h1>
+          </div>
+        })}
+      </div>
+    );
+  }
+
 }
 
 export default App;
